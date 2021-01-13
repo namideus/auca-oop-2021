7b7f3dc1052d8a0c5735ff959ed6e5359a4f4c0a
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-01-13 13:04:26 +0600

    URI problem 1332 solved

---
+++ m-uri-1332/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String d,r;
+        while(t>0)
+        {
+            d = sc.next();
+            if(d.length()==5)
+                r = "3";
+            else if(d.charAt(0)=='t' && d.charAt(1)=='w' ||
+                    d.charAt(0)=='t' && d.charAt(2)=='o' ||
+                    d.charAt(1)=='w' && d.charAt(2)=='o')
+                r = "2";
+            else r = "1";
+            System.out.println(r);
+            t--;
+        }
+    }
+}
