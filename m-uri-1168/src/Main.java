e753dd6ee052200bc390ea4683dd7567d95968cc
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-01-13 13:23:13 +0600

    URI problem 1168 solved

--- m-uri-1168/src/Main.java
+++ m-uri-1168/src/Main.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-
-       /* int t = sc.nextInt();
-        String d,r;
-        while(t>0)
-        {
-            d = sc.next();
-            if(d.length()==5)
-                r = "3";
-            else if(d.charAt(0)=='t' && d.charAt(1)=='w' ||
-                    d.charAt(0)=='t' && d.charAt(2)=='0' ||
-                    d.charAt(1)=='w' && d.charAt(2)=='0')
-                r = "2";
-            else r = "1";
-            System.out.println(r);
-            t--;
-        }*/
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String str;
+        char ch;
+        int leds;
+        for (int i = 1; i <= t; i++) {
+            str = s.next();
+            leds = 0;
+            for (int j = 0; j < str.length(); j++) {
+                ch = str.charAt(j);
+                if (ch == '0' || ch == '6' || ch == '9')
+                    leds += 6;
+                if (ch == '1')
+                    leds += 2;
+                if (ch == '2' || ch == '3' || ch == '5')
+                    leds += 5;
+                if (ch == '4')
+                    leds += 4;
+                if (ch == '7')
+                    leds += 3;
+                if (ch == '8')
+                    leds += 7;
+            }
+            System.out.println(leds + " leds");
+        }
     }
 }
