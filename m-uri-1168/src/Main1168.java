ee841113b31cabb070f4bd2880f3f08954125264
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-01-13 14:31:39 +0600

    URI problem 1768 presentation error % 35

--- m-uri-1168/src/Main1168.java
+++ m-uri-1168/src/Main1168.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main1168 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
---
+++ m-uri-1768/src/Main1768.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main1768 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n, i, j;
+
+        while(s.hasNext())
+        {
+            n = s.nextInt();
+
+            for(i=1; i<=n; i+=2)
+            {
+                for(j = n-i-1; j>=0 ; j--)
+                    System.out.print(" ");
+
+                for(j=0; j<i; j++)
+                    System.out.print("* ");
+
+                System.out.println();
+            }
+
+            for(i=n/2; i>=1; i--)
+                System.out.print("  ");
+            System.out.print("*\n");
+
+            for(i=n/2-1; i>=1; i--)
+                System.out.print("  ");
+            System.out.print("* * *\n");
+
+            System.out.println();
+        }
+    }
+}
