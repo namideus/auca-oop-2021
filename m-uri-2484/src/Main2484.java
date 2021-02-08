33ed4ea01879a428f08d4691f235df6805f87550
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-02-08 20:22:37 +0600

    URI 2484 TME

---
+++ m-uri-2484/src/Main2484.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main2484 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i,j,l,r;
+        String str;
+
+        while(scanner.hasNext())
+        {
+            str = scanner.next();
+
+            l = str.length();
+            r = str.length();
+
+            for(int count = 0; count<l; ++count)
+            {
+                for(j=0; j<count; ++j)
+                    System.out.print(" ");
+
+                for(i=0; i<l-count; ++i)
+                    System.out.print(str.charAt(i)+((i!=(l-count)-1) ? " ": ""));
+
+                System.out.println();
+            }
+
+            System.out.println();
+
+        }
+    }
+}
