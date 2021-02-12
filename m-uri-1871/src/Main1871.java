0c8612d5472432d287914af2a9c7d5703b833f5f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-02-12 14:50:23 +0600

    URI 1871 accepted

---
+++ m-uri-1871/src/Main1871.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main1871 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i,a,b,s;
+        String q;
+
+        while(scanner.hasNextInt())
+        {
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+
+            if(a==0 && b==0) break;
+
+            s = a+b;
+            q = s+"";
+
+            for(i=0; i<q.length(); ++i) {
+                if (q.charAt(i) != '0')
+                    System.out.print(q.charAt(i));
+            }
+
+            System.out.println();
+        }
+    }
+}
