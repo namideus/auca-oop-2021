6468f59058d5c0d0ea3327f081edd5455dbcd684
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-02-27 09:20:16 +0600

    URI 1873 accepted

---
+++ m-uri-1873/src/Main1873.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main1873 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        scan.nextLine();
+        String a,b;
+        while(t>0) {
+            a = scan.next();
+            b = scan.next();
+
+            if(a.equals(b))
+                System.out.println("empate");
+            else if(a.equals("tesoura")&&b.equals("papel")||
+                    a.equals("papel")&&b.equals("pedra")||
+                    a.equals("pedra")&&b.equals("lagarto")||
+                    a.equals("lagarto")&&b.equals("spock")||
+                    a.equals("spock")&&b.equals("tesoura")||
+                    a.equals("tesoura")&&b.equals("lagarto")||
+                    a.equals("lagarto")&&b.equals("papel")||
+                    a.equals("papel")&&b.equals("spock")||
+                    a.equals("spock")&&b.equals("pedra")||
+                    a.equals("pedra")&&b.equals("tesoura"))
+                System.out.println("rajesh");
+            else
+                System.out.println("sheldon");
+            --t;
+        }
+    }
+}
