467cc085cde53b774bed36f63fd0efa88f5f5e1f
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-08 17:55:36 +0600

    lab-04 created ArrayListInt class

---
+++ lab-04/src/ArrayClassInt.java
@@ -0,0 +1,40 @@
+public class ArrayClassInt {
+    public ArrayClassInt() {
+
+    }
+
+    public int size()
+    {
+        return 0;
+    }
+
+    public int get(int i)
+    {
+        return 0;
+    }
+
+    public void set(int i, int v)
+    {
+
+    }
+
+    public void add(int e)
+    {
+
+    }
+
+    public void add(int i, int v)
+    {
+
+    }
+
+    public void remove(int i)
+    {
+
+    }
+
+    public String toString()
+    {
+        return null;
+    }
+}
