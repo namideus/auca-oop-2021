93a18ddb30b556993e34efd66c2219fbef809492
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-21 08:30:00 +0600

    Implemented recursive gcd in project-01

---
+++ project-01/src/Problem02.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter m: ");
+        int m = scan.nextInt();
+        System.out.print("Enter n: ");
+        int n = scan.nextInt();
+        System.out.println(gcd(m,n));
+    }
+
+    public static long gcd(long m, long n) {
+        if(m%n==0)
+            return n;
+        else
+            return gcd(n, m%n);
+    }
+}
