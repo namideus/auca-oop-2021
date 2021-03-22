b88ab8f12ec69eb466e19604d96c602cf5eae40e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-22 16:08:32 +0600

    Uri final 1574 accepted

---
+++ m-uri-final-1285/src/Main1285.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main1285 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n, m,c,mx,num;
+        int[] cnt;
+
+        while(scan.hasNextInt()) {
+            n = scan.nextInt();
+            m = scan.nextInt();
+            c = 0;
+
+            for (int d = n; d <= m; d++) {
+                cnt = new int[5001];
+                mx = 0;
+                num = d;
+
+                while (num > 0) {
+                    cnt[num % 10]++;
+                    mx = Math.max(mx, cnt[num % 10]);
+                    num /= 10;
+                }
+
+                if (mx == 1)
+                    c++;
+            }
+            System.out.println(c);
+        }
+    }
+}
---
+++ m-uri-final-1574/src/Main1574.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main1574 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String cmd,a,b;
+        ArrayList<String> list;
+        int t,l,r,i,n,pos;
+        t = scan.nextInt();
+        scan.nextLine();
+
+        while(t>0) {
+            l  = r = 0;
+            n = scan.nextInt();
+
+            list = new ArrayList<>();
+
+            for(i=0; i<n; ++i)
+            {
+                cmd = scan.next();
+
+                if(cmd.equals("LEFT") || cmd.equals("RIGHT"))
+                    list.add(cmd);
+                else {
+                    scan.next();
+                    pos = scan.nextInt();
+                    list.add(list.get(pos-1));
+                }
+            }
+
+            for(i=0; i<n; i++)
+                if(list.get(i).equals("LEFT"))
+                    ++l;
+                else
+                    ++r;
+
+            System.out.println(r-l);
+
+            --t;
+        }
+    }
+}
