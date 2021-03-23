f1f20308830734ed21cab5e7fef754be26c4305e
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-23 11:58:38 +0600

    Uri final 1136 accepted

---
+++ m-uri-final-1136/src/Main1136.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+import static java.lang.Math.abs;
+
+public class Main1136 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i,j,n,b,c;
+
+        while(scan.hasNextInt()) {
+            n = scan.nextInt();
+            b = scan.nextInt();
+
+            if(n+b==0)
+                break;
+
+            int[] arr = new int[b];
+            int[] cnt = new int[n+1];
+
+            c=0;
+
+            for(i=0; i<b; i++)
+                arr[i] = scan.nextInt();
+
+            for(i=0; i<b; i++)
+                for(j=0; j<b; j++)
+                    ++cnt[abs(arr[i]-arr[j])];
+
+            for(i=0; i<=n; i++)
+                if(cnt[i]>0)
+                    c++;
+
+            System.out.println((c==n+1) ? "Y" : "N");
+        }
+    }
+}
