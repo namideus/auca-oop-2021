a3749669f7ea6eff7b1079395144c593316c6faa
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-23 11:39:15 +0600

    Uri final 1196 accepted

---
+++ m-uri-final-1196/src/Main1196.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main1196 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i,j,n,t;
+        String s, alph = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+        char c;
+
+        while(scan.hasNextLine())
+        {
+            s = scan.nextLine();
+
+            for(i=0; i<s.length(); i++)
+            {
+                c = s.charAt(i);
+
+                if(c!=' ')
+                    System.out.print(alph.charAt(alph.indexOf(c)-1));
+
+                if(c==' ' || c=='`')
+                    System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+}
