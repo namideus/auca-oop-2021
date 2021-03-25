0bf8d8601b4b543f9c6391401b04c6e7d68e4155
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-25 15:48:05 +0600

    Uri final 1533 accepted

---
+++ m-uri-final-1533/src/Main1533.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class Main1533 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n;
+        Map<Integer, Integer> map;
+
+        while(scan.hasNextInt()) {
+            n = scan.nextInt();
+
+            if(n==0) break;
+
+            map = new TreeMap<>();
+
+            for(int i=1; i<=n; i++)
+                map.put(scan.nextInt(), i);
+
+            ArrayList<Integer> list = new ArrayList(map.keySet());
+            System.out.println(map.get(list.get(n-2)).intValue());
+        }
+    }
+}
