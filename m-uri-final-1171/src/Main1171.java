7cc0342def03dc0c7bfbc125cbcb3cc0ae5d9a0a
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-03-25 15:05:07 +0600

    Uri final 1171 accepted

---
+++ m-uri-final-1171/src/Main1171.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main1171 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[2001];
+        int n, t = scan.nextInt();
+        scan.nextLine();
+        Set<Integer> set = new HashSet<>();
+
+        while(t>0) {
+            n = scan.nextInt();
+            set.add(n);
+            ++arr[n];
+            --t;
+        }
+        List<Integer> list = new ArrayList<>(set);
+        list.sort(null);
+        for(int i : list)
+            System.out.printf("%d aparece %d vez(es)\n", i, arr[i]);
+    }
+}
