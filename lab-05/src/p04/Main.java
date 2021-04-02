e25d4635f3a225fe80255ed8e96e495c82e06778
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-04-02 11:48:50 +0600

    Solved problem 4 form lab-05: drew a chessboard pattern

---
+++ lab-05/src/p04/Main.java
@@ -0,0 +1,49 @@
+package p04;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class Main extends JFrame {
+
+    Main() {
+        setTitle("Chessboard");
+        setLayout(new GridLayout(8,8,0,0));
+        // 8x8 squares
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                boolean isBlack = (col % 2 == 0) == (row % 2 == 0);
+                add(new DrawRect(isBlack ? 0 : 1));
+            }
+        }
+    }
+
+    static class DrawRect extends JPanel {
+        private int colorCode = 0;
+
+        public DrawRect(int c) {
+            colorCode = c;
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+
+            if (colorCode==0)
+                g.setColor(Color.BLACK);
+            else
+                g.setColor(Color.WHITE);
+
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
+    }
+
+    public static void main(String[] args) {
+        Main frame = new Main();
+        frame.setSize(600,600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
