abe73b0833522a6c053504279eccc995e60e9ec4
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-04-09 10:50:57 +0600

    Beginning to develop GUI minesweeper, drew digit labels, added texts

--- project-01/src/MinesweeperUIMain.java
+++ project-01/src/MinesweeperUIMain.java
@@ -2,6 +2,8 @@ import processing.core.*;
 
 public class MinesweeperUIMain extends PApplet {
 
+    private String mode;
+
     public void settings() {
         fullScreen();
     }
@@ -12,6 +14,42 @@ public class MinesweeperUIMain extends PApplet {
 
     public void draw() {
         background(0, 0, 0);
+
+        float x = width/2f+200f;
+        float y = 250f;
+        float w = 190f;
+        float h = 40f;
+        float dif = 70f;
+
+        //-----------SMILE-------------------------
+        drawSmile(false);
+        //----------------------------------------------
+        textSize(40);
+        fill(255, 0, 0);
+        text("008",width/2f-130f, 165f);
+        text("006",width/2f+50f, 165f);
+
+        fill(169,169,169);
+        rect(x,y,w,h);
+        onClick(x,y,w,h,0, MinesweeperGame.BEGINNER);
+
+        fill(169,169,169);
+        rect(x,y+dif,w,h);
+        onClick(x,y,w,h,dif, MinesweeperGame.INTERMEDIATE);
+
+        fill(169,169,169);
+        rect(x,y+2*dif,w,h);
+        onClick(x,y,w,h,2*dif, MinesweeperGame.EXPERT);
+
+        textSize(20);
+        fill(0, 255, 0);
+        text(MinesweeperGame.BEGINNER,x+50f, y+27f);
+        text(MinesweeperGame.INTERMEDIATE,x+30f, y+dif+27f);
+        text(MinesweeperGame.EXPERT,x+60f, y+2*dif+27f);
+    }
+
+    public void drawSmile(boolean sad) {
+        //-----------SMILE-------------------------
         //yellow
         fill(255, 255, 0);
         //draw the head
@@ -32,29 +70,13 @@ public class MinesweeperUIMain extends PApplet {
         strokeWeight(2f);
         arc(width/2f, 157, 25, 18, 0, PI);
         //arc(width/2f, 115, 25, 18, -PI, 0);
-
-        float x = width/2f+200f;
-        float y = 250f;
-        float w = 190f;
-        float h = 40f;
-        float dif = 70f;
-
-        fill(255,255,255);
-        rect(x,y,w,h);
-        onClick(x,y,w,h,0, "beginner");
-
-        fill(255,255,255);
-        rect(x,y+dif,w,h);
-        onClick(x,y,w,h,dif, "intermediate");
-
-        fill(255,255,255);
-        rect(x,y+2*dif,w,h);
-        onClick(x,y,w,h,2*dif, "expert");
+        //----------------------------------------------
     }
 
     public void onClick(float x, float y, float w, float h, float dif, String level) {
         if(mousePressed) {
             if (mouseX > x && mouseX < x + w && mouseY > y + dif && mouseY < y + dif + h) {
+                this.mode = level;
                 fill(GRAY);
                 rect(x,y+dif,w,h);
                 //do stuff
