ed95e3e2a2aeb004867c805113899249eb075ab3
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-04-20 22:36:43 +0600

    f-uri-1514 accepted

--- f-uri-1514/src/Main1514.java
+++ f-uri-1514/src/Main1514.java
@@ -1,5 +1,82 @@
-public class Main1514 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main1514 {
+    private static int m, n, i, j;
+
+    static int cond_one(int[][] a) {
+        int c;
+
+        for(i=0;i<n;++i) {
+            c = 0;
+
+            for(j=0;j<m;++j)
+                c += a[i][j];
+
+            if(c==m)
+                return 0;
+        }
+        return 1;
+    }
+
+    static int cond_two(int[][] a) {
+        int c=0, b=1, b1;
+
+        for(i=0; i<m; ++i) {
+            b1 = 0;
+            for(j=0; j<n; ++j)
+                if(a[j][i]==1)
+                    b1 = 1;
+
+            if(b1==1)
+                ++c;
+        }
+
+        return (c==m)? 1 : 0;
+    }
+
+    static int cond_three(int[][] a) {
+        int c=0, b=1;
+
+        for(i=0; i<m; ++i) {
+            c = 0;
+            for(j=0; j<n; ++j)
+                c += a[j][i];
+            if(c==n)
+                return 0;
+        }
+
+        return 1;
+    }
+
+    static int cond_four(int[][] a) {
+        int c = 0;
+
+        for(i=0; i<n; ++i) {
+            for(j=0; j<m; ++j)
+                if(a[i][j]==1) {
+                    c++;
+                    break;
+                }
+        }
+
+        return (c==n)? 1 : 0;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[][] a = new int[101][101];
+        while(scan.hasNextInt()) {
+            n = scan.nextInt();
+            m = scan.nextInt();
+
+            if(n+m==0)
+                break;
+
+            for(i=0;i<n;++i) {
+                for (j = 0; j < m; ++j)
+                    a[i][j] = scan.nextInt();
+            }
+
+            System.out.println(cond_one(a) + cond_two(a) + cond_three(a) + cond_four(a));
+        }
     }
 }
