fc54a8fe14e60133f11534036c8dc3a4461bc260
Altynbek uulu Yiman <altynbek_y@auca.kg>
2021-05-01 17:27:34 +0600

    lab08: task 4 implemented

---
+++ lab-08/src/p04/ReplaceText.java
@@ -0,0 +1,37 @@
+package p04;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class ReplaceText {
+    public static void main(String[] args) throws Exception {
+        if(args.length!=4) {
+            System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
+            System.exit(1);
+        }
+
+        File sourceFile = new File(args[0]);
+        if(!sourceFile.exists()) {
+            System.out.println("Source file " + args[0] + " does not exist");
+            System.exit(2);
+        }
+
+        File targetFile = new File(args[1]);
+        if(targetFile.exists()) {
+            System.out.println("Target file " + args[1] + " already exists");
+            System.exit(3);
+        }
+
+        try (
+            Scanner scan = new Scanner(sourceFile);
+            PrintWriter output = new PrintWriter(targetFile);
+        ) {
+            while (scan.hasNext()) {
+                String s1 = scan.nextLine();
+                String s2 = s1.replaceAll(args[2], args[3]);
+                output.println(s2);
+            }
+        }
+    }
+}
